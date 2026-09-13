9a7d8227fec0381dce9d5842c1ed35a206a5b8f7
azharul1110 <kaziazharuli1@gmail.com>
2026-09-13 22:20:20 +0600

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/b14-Fifth-assignment/',
   plugins: [react(), tailwindcss()],
 })
